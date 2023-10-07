ebefbcfe7160b615ad28570c33d46bd749f6136a
lucasherlon <lucasherlondsmc@gmail.com>
2023-10-07 17:30:11 -0300

    adding merge and insertion

--- algoritmos/Sorting.cs
+++ algoritmos/Sorting.cs
@@ -23,4 +23,55 @@ internal class Sorting
         }
     }
 
+    public static void QuickSort(int[] array, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivot = Partition(array, left, right);
+
+            if (pivot > 1)
+            {
+                QuickSort(array, left, pivot - 1);
+            }
+            if (pivot + 1 < right)
+            {
+                QuickSort(array, pivot + 1, right);
+            }
+        }
+    }
+
+    public static int Partition(int[] array, int left, int right)
+    {
+        int pivot = array[left];
+
+        while (true)
+        {
+            while (array[left] < pivot)
+            {
+                left++;
+            }
+
+            while (array[right] > pivot)
+            {
+                right--;
+            }
+
+            if (left < right)
+            {
+                if (array[left] == array[right])
+                {
+                    return right;
+                }
+
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+            else
+            {
+                return right;
+            }
+        }
+    }   
+
 }
